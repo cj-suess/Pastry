d0403ebeea8f23f9299871c84e62d69d71f8b50a
Cameron Suess <camsuess@antlion.cs.colostate.edu>
2025-10-10 09:47:07 -0600

    started leafset implementation

--- app/src/main/java/csx55/pastry/util/Leafset.java
+++ app/src/main/java/csx55/pastry/util/Leafset.java
@@ -1,5 +1,21 @@
 package csx55.pastry.util;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import csx55.pastry.wireformats.PeerInfo;
+
 public class Leafset {
+    
+    List<PeerInfo> neighbors = Collections.synchronizedList(new ArrayList<>());
+
+    public Leafset() {
+
+    }
+
+    public List<PeerInfo> getLeafSet(){
+        return neighbors;
+    }
     
 }
